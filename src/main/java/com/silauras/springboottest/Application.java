c17711a7f1e1fe565600294a9dfe20f2adb8d74f
silauras <olegpensov@gmail.com>
2021-01-16 23:32:39 +0200

    Init Commit

---
+++ src/main/java/com/silauras/springboottest/Application.java
@@ -0,0 +1,16 @@
+package com.silauras.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Silauras
+ * Created on 16.01.2021 at 23:12
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
