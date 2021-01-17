bdb0fd964a25846e36097842ca7ce6cfd8e24432
silauras <olegpensov@gmail.com>
2021-01-17 23:52:14 +0200

    Change template engine to Freemarker

--- src/main/java/com/silauras/springboottest/controller/MainController.java
+++ src/main/java/com/silauras/springboottest/controller/MainController.java
@@ -3,8 +3,10 @@ package com.silauras.springboottest.controller;
 import com.silauras.springboottest.domain.Message;
 import com.silauras.springboottest.domain.User;
 import com.silauras.springboottest.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,11 +21,8 @@ import java.util.Map;
 @Controller
 public class MainController {
 
-    private final MessageRepo messageRepo;
-
-    public MainController(MessageRepo messageRepo) {
-        this.messageRepo = messageRepo;
-    }
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -31,10 +30,16 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty())
+            messages = messageRepo.findByTag(filter);
+        else
+            messages = messageRepo.findAll();
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
 
         return "main";
     }
@@ -44,26 +49,14 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model){
-
+            Map<String, Object> model
+    ){
         Message message = new Message(text, tag, user);
+
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter,
-                         Map<String, Object> model){
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        }else {
-            messages = messageRepo.findAll();
-        }
         model.put("messages", messages);
         return "main";
     }
